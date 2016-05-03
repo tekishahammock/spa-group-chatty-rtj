7429f2c2e659cbd63771ce1860f89e84c88199c6
Russell Ries <riesru@gmail.com>
2016-05-03 15:03:46 -0500

    add incomplete checkbox event listeners

---
+++ js/chatty.js
@@ -0,0 +1,15 @@
+// linking to body for theme changes from checkboxes (incomplete)
+var body = document.getElementById("");
+
+// adding event listener to the Dark Theme button (incomplete)
+var darkTheme = document.getElementById("darkTheme");
+darkTheme.addEventListener("click", function(event) {
+  body.classList.toggle("");
+});
+
+// adding event listener to the Large Text button (incomplete)
+var largeText = document.getElementById("largeText");
+largeText.addEventListener("click", function(event) {
+  body.classList.toggle("");
+});
+
