185fadfe97f4683881084a00fc0a06e6b0f8b648
Russell Ries <riesru@gmail.com>
2016-05-04 14:41:46 -0500

    fixed clear all button issues

--- js/deleteiife.js
+++ js/deleteiife.js
@@ -2,11 +2,15 @@ var chatty = (function(chatty) {
 
   chatty.deleteMessage = function(clickEvent) {
     console.log(clickEvent);
+    console.log(clickEvent.target.parentNode)
     removeElement.removeChild(clickEvent.target.parentNode);
   }
 
   chatty.clearAllMessages = function(clickEvent) {
-    removeElement.parentNode.removeChild(removeElement);
+    console.log(removeElement);
+    while (removeElement.firstChild) {
+      removeElement.removeChild(removeElement.firstChild);
+    }
     chatty.clearArray();
   }
 
