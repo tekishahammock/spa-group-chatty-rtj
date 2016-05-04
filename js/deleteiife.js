f3aa11aec9bad2c5ccef83d9760976ea440e678e
Russell Ries <riesru@gmail.com>
2016-05-04 10:54:37 -0500

    add individual delete function

--- js/deleteiife.js
+++ js/deleteiife.js
@@ -1,5 +1,11 @@
-var remove = (function(chatty) {
+var chatty = (function(chatty) {
 
+  chatty.deleteMessage = function(clickEvent) {
+    console.log(clickEvent);
+    removeElement.removeChild(clickEvent.target.parentNode);
+  }
+
+  return chatty
 }( chatty || {}));
 
 
