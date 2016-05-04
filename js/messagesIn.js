b4d75ca53e6132cbddf0a3e7ffeee7c67c593fb9
set2k6 <set2k6@gmail.com>
2016-05-04 15:27:09 -0500

    lets see

--- js/messagesIn.js
+++ js/messagesIn.js
@@ -1,16 +1,19 @@
 var chatty = (function(originalChatty) {
 
   var messageArray = [];
-	var outputDOM = document.getElementById("chatbox");
+  var outputDOM = document.getElementById("chatbox");
 
   originalChatty.userInputMessages = function(userMess) {
-  var userInput = document.getElementById("userInput").value;
-  messageArray.push(userInput);
+  var userInput = document.getElementById("userInput");
+  var userStuff = userInput.value
+  messageArray.push(userStuff);
+  userInput.value = "";
   console.log(messageArray);
   for (var i = 0; i < messageArray.length; i++) {
-      outputDOM += `<div id="chatbox">`;
-      outputDOM += `{userInput.messsageArray}`;
-	}
+    console.log(messageArray[i]);
+    var currentMessage = messageArray[i];
+      outputDOM.innerHTML += `<div class="userMessage">${currentMessage}<button type="delete" id="delete[i]" class="delete">delete</button></div>`;
+  }
 }
 
   originalChatty.clearArray = function() {
