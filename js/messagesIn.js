a384e1b385f95aa42c8cdd3abf38f503c4840ea4
set2k6 <set2k6@gmail.com>
2016-05-04 15:22:32 -0500

    testing

--- js/messagesIn.js
+++ js/messagesIn.js
@@ -4,7 +4,6 @@ var chatty = (function(originalChatty) {
 	var outputDOM = document.getElementById("chatbox");
 
   originalChatty.userInputMessages = function(userMess) {
-<<<<<<< HEAD
   var userInput = document.getElementById("userInput").value;
   messageArray.push(userInput);
   console.log(messageArray);
